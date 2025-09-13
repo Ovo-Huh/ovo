7efd0d8ef61cf5cc89d4627a516979acffb03d79
Ovo-Huh <2258948547@qq.com>
2025-09-13 11:01:41 +0800

    Delete self directory

--- self/ovo.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int i;
-    i=10;
-    printf("i=%m.4d",i);
-    return 0;
-} nknknk
--- self/wwww.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-    int i,j;
-    float x,y;
-    scanf("%d%d%f%f",&i,&j,&x,&y);
-    printf(i,j,x,y);
-    return 0;
-}
